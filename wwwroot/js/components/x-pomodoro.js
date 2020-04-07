15d3e4110a4532ce2cf734b55b6dffb6eb394571
Allan Oloo <allanjoloo@me.com>
2020-04-07 19:41:15 -0400

    Project Front end architecture setup. Added project depencies.

---
+++ wwwroot/js/components/x-pomodoro.js
@@ -0,0 +1,16 @@
+﻿xtag.register('x-pomodoro', {
+    lifecycle: {
+        created: function () {
+            alert('I fire when an ‹x-foo› is CREATED');
+        },
+        inserted: function () {
+            alert('I fire when an ‹x-foo› is INSERTED to the DOM');
+        },
+        removed: function () {
+            alert('I fire when an ‹x-foo› is REMOVED from the DOM');
+        },
+        attributeChanged: function (attrName, oldValue, newValue) {
+            alert('I fire when an ATTRIBUTE is CHANGED on an ‹x-foo›');
+        }
+    },
+});
---
+++ wwwroot/js/setup.js
@@ -0,0 +1,8 @@
+﻿requirejs.config({
+    baseUrl: "js",
+    paths: {
+        "xtag": "xtag/xtag",
+        "x-pomodoro": "components/x-pomodoro"
+    },
+    waitSeconds: 15
+});
